1fe33cfede9681140080e185e8aff977eec8aeea
sulaga <sulaga@gmx.ch>
2022-07-08 16:31:06 +0200

    Bug fix for two elements

--- prose/join.go
+++ prose/join.go
@@ -6,7 +6,7 @@ func JoinWithCommas(phrases []string) string {
 	if len(phrases) == 1 {
 		return phrases[0]
 	} else if len(phrases) == 2 {
-		return phrases[1] + " and " + phrases[1]
+		return phrases[0] + " and " + phrases[1]
 	} else {
 		result := strings.Join(phrases[:len(phrases)-1], ", ")
 		result += ", and "
